e9b6aa122e9c7156f7810b7a9455cf93a040444c
UserName <Email>
2021-07-17 04:09:42 -0400

    Add project files.

---
+++ LuggerWPF/Item.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using LuggerWPF.Annotations;
+
+namespace LuggerWPF
+{
+    /// <summary>
+    /// This actually represents "assembly"
+    /// </summary>
+    public class Item : INotifyPropertyChanged
+    {
+        //Let's have one rectangle which contains multiple circles
+        private double _percent;
+        private string _name;
+        public ObservableCollection<IShape> Shapes { get; set; } = new ObservableCollection<IShape>();
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        public double Percent
+        {
+            get => _percent;
+            set { _percent = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public interface IShape
+    {
+        double Width { get; set; }
+        double Height { get; set; }
+        double X { get; set; }
+        double Y { get; set; }
+        double Radius { get; set; }
+    }
+
+    public class Rectangle : INotifyPropertyChanged, IShape
+    {
+        private double _y;
+        private double _x;
+        private double _height;
+        private double _width;
+
+
+        public double Width
+        {
+            get => _width;
+            set { _width = value; OnPropertyChanged(); }
+        }
+
+        public double Height
+        {
+            get => _height;
+            set { _height = value; OnPropertyChanged(); }
+
+        }
+        public double X
+        {
+            get => _x;
+            set { _x = value; OnPropertyChanged(); }
+        }
+
+        public double Y
+        {
+            get => _y;
+            set { _y = value; OnPropertyChanged(); }
+        }
+
+        public double Radius { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Circle : INotifyPropertyChanged, IShape
+    {
+        private double _y;
+        private double _x;
+        private double _radius;
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+
+        public double X
+        {
+            get => _x;
+            set { _x = value; OnPropertyChanged(); }
+        }
+
+        public double Y
+        {
+            get => _y;
+            set { _y = value; OnPropertyChanged(); }
+        }
+
+        public double Radius
+        {
+            get => _radius;
+            set { _radius = value; OnPropertyChanged(); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ LuggerWPF/MainVm.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using LuggerWPF.Annotations;
+
+namespace LuggerWPF
+{
+    public class MainVm : INotifyPropertyChanged
+    {
+        private Item _selectedItem;
+        public ObservableCollection<Item> Items { get; set; } = new ObservableCollection<Item>();
+
+
+        public Item SelectedItem
+        {
+            get => _selectedItem;
+            set { _selectedItem = value; OnPropertyChanged(); }
+        }
+
+        public MainVm()
+        {
+            Items.Add(new Item() { Name = "Big 1", Percent = 0.30 });
+            Items.Add(new Item()
+            {
+                Name = "Big 2",
+                Percent = 0.40,
+                Shapes = new ObservableCollection<IShape>(
+                    new List<IShape>()
+                    {
+                        new Rectangle()
+                        {
+                            X = 25,
+                            Y = 25,
+                            Height = 30,
+                            Width = 30
+                        }, new Circle()
+                        {
+                            Radius = 20,
+                            X = 90,
+                            Y=20
+                        }
+                    })
+            });
+            Items.Add(new Item() { Name = "Big 3", Percent = 0.50 });
+            if (Items.Count != 0)
+            {
+                SelectedItem = Items.First();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
