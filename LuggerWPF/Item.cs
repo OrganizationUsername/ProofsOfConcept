c90efce84a7ed1bf46296b9b5e19e12bfbb5b32e
UserName <Email>
2021-07-17 15:17:15 -0400

    working on items.

--- LuggerWPF/Item.cs
+++ LuggerWPF/Item.cs
@@ -27,7 +27,7 @@ namespace LuggerWPF
         public double Thickness
         {
             get => _thickness;
-            set => _thickness = value;
+            set { _thickness = value; OnPropertyChanged(); }
         }
 
         public double Percent
@@ -35,6 +35,39 @@ namespace LuggerWPF
             get => _percent;
             set { _percent = value; OnPropertyChanged(); OnPropertyChanged(nameof(Passes)); }
         }
+
+
+
+        /// <summary>
+        /// This is the function that will set Percent. It really should be invoked in another way.
+        /// </summary>
+        public void Calculate()
+        {
+            foreach (var shape in Shapes)
+            {
+                if (shape is Rectangle rectangle)
+                {
+                    //TODO add these two types to a List<T> then do the math.
+                    //Think about how this will be done in batch
+                    //Create plain CTOR.
+                    //Also create one that can be easily overridden for Parallel.For
+                }
+                else if (shape is Circle cirlCircle)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+
+
+            Percent = 1.0;
+        }
+
+
+
 
         public event PropertyChangedEventHandler PropertyChanged;
 
